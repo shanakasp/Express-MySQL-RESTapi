f93f3b57d408fb61b94c12b46a9b99f68b58108b
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-04-23 19:11:11 +0530

    modified index.js in code

--- code/index.js
+++ code/index.js
@@ -96,3 +96,21 @@ app.post("/tickets", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
+app.get("/tickets/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const sql = "SELECT * FROM tickets WHERE id = ?";
+
+    const result = await queryPromise(sql, [id]);
+    if (result.length === 0) {
+      return res.status(404).json({ error: "Ticket not found" });
+    }
+
+    res.status(200).json(result[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
